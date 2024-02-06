e3b9f144254249e4565bfffc50905bbe9d980dd1
John Kyle Salaysay <foursalaysay@gmail.com>
2024-02-07 07:11:06 +0800

    add login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,44 +1,69 @@
-import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
+// import { cn } from "@/lib/utils";
+// import { buttonVariants } from "@/components/ui/button";
+// import LoginForm from "./components/LoginForm";
+// import { getServerSession } from "next-auth";
+// import { authConfig } from "@/lib/auth.config";
 
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
-import LoginForm from "./components/LoginForm";
-import { getServerSession } from "next-auth";
-import { authConfig } from "@/lib/auth.config";
+import { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/registry/new-york/ui/button"
+import { UserAuthForm } from "@/app/examples/authentication/components/user-auth-form"
 
 export const metadata: Metadata = {
-  title: "Log in",
-  description:
-    "Log in to your account to access exclusive features and content.",
-};
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+}
 
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="md:hidden">
+        <Image
+          src="/examples/authentication-light.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/authentication-dark.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          href="/register"
+          href="/examples/authentication"
           className={cn(
             buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-4 md:top-4"
+            "absolute right-4 top-4 md:right-8 md:top-8"
           )}
         >
-          Register
+          Login
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10  dark:border-r lg:flex">
-          <div className="absolute inset-0 ">
-            <Image src="login.svg" alt="login image" objectFit="contain" fill />
-          </div>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <h1 className="text-2xl font-black">
-              <Link href={"/"}>
-                <span className="text-primary">MiJo </span> Express
-              </Link>
-            </h1>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Acme Inc
           </div>
-          {/* <div className="relative z-20 mt-auto">
+          <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
                 &ldquo;This library has saved me countless hours of work and
@@ -47,19 +72,19 @@ export default function AuthenticationPage() {
               </p>
               <footer className="text-sm">Sofia Davis</footer>
             </blockquote>
-          </div> */}
+          </div>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] lg:w-[450px]">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Welcome back!
+                Create an account
               </h1>
               <p className="text-sm text-muted-foreground">
-                Log in to your account to access exclusive features and content.
+                Enter your email below to create your account
               </p>
             </div>
-            <LoginForm />
+            <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               <Link
@@ -81,5 +106,5 @@ export default function AuthenticationPage() {
         </div>
       </div>
     </>
-  );
+  )
 }
