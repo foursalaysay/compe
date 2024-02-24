ba0415d5448d36aa843fb22c83b509fa1d25cb10
John Kyle Salaysay <foursalaysay@gmail.com>
2024-02-25 01:42:02 +0800

    addinng layers

--- src/app/dashboard/tracker/page.tsx
+++ src/app/dashboard/tracker/page.tsx
@@ -1,6 +1,11 @@
-export default function TrackingPage(){
-    return(
-        <div>
-        </div>
-    )
+import React from 'react'
+
+import SearchBar from './components/search-bar'
+
+export default function TrackingPage() {
+  return (
+    <div className='w-full'>
+        <SearchBar />
+    </div>
+  )
 }
