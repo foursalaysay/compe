bcab2945c8273fb34946959b997088306173dc8e
John Kyle Salaysay <foursalaysay@gmail.com>
2024-03-01 08:22:50 +0800

    redefined

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -64,9 +64,9 @@ export default function AuthenticationPage() {
             <blockquote className="space-y-2">
               
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;this app help companies to share and distribute their 
+                donations to deserving organizations who most need their help.
+                &rdquo;
               </p>
               <footer className="text-sm">Sofia Davis</footer>
             </blockquote>
